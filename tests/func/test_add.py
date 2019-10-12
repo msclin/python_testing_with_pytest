5998fe5773fe311117cb3e171192105028fcc7a4
Marc Clinedinst <msclin@clinedim.local>
2019-10-12 17:49:42 -0400

    Adding first real test.

--- tests/func/test_add.py
+++ tests/func/test_add.py
@@ -1,6 +1,1 @@
-import logging
-
-
-def test_add():
-    logging.info('Verifying that 1 + 1 equals 2.')
-    assert 1 + 1 == 2
+"""Placeholder file."""
--- tests/unit/test_task.py
+++ tests/unit/test_task.py
@@ -1,1 +1,14 @@
-"""Placeholder test file."""
+from tasks import Task
+
+
+def test_asdict():
+    t_task = Task('do something', 'okken', True, 21)
+    t_dict = t_task._asdict()
+    expected = {
+        'summary': 'do something',
+        'owner': 'okken',
+        'done': True,
+        'id': 21
+    }
+
+    assert t_dict == expected
