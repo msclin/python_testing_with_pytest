6dc9d100faca258f013f8600f04d8c1e0a7358e4
Marc Clinedinst <msclin@clinedim.local>
2019-10-12 19:17:06 -0400

    Fixed test failures.

--- tests/unit/test_task.py
+++ tests/unit/test_task.py
@@ -48,7 +48,7 @@ def test_task_equality():
     t1 = Task('sit there', 'brian')
     t2 = Task('do something', 'okken')
 
-    assert t1 == t2
+    assert t1 != t2
 
 
 def test_dict_equality():
@@ -56,4 +56,4 @@ def test_dict_equality():
     t1_dict = Task('make sandwich', 'okken')._asdict()
     t2_dict = Task('make sandwich', 'okkem')._asdict()
 
-    assert t1_dict == t2_dict
+    assert t1_dict != t2_dict
