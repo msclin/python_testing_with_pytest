a771728ce1d9ad50c6dead1aee8bec3d6d50c4c9
Marc Clinedinst <msclin@clinedim.local>
2019-10-12 19:14:41 -0400

    Adding two failing tests.

--- tests/unit/test_task.py
+++ tests/unit/test_task.py
@@ -26,7 +26,7 @@ def test_replace():
 
 
 def test_defaults():
-    """Check .field functionality of namedtuple."""
+    """Using no parameters should invoke defaults."""
     t1 = Task()
     t2 = Task(None, None, False, None)
 
@@ -34,9 +34,26 @@ def test_defaults():
 
 
 def test_member_access():
+    """Check .field functionality of namedtuple."""
     t = Task('buy milk', 'brian')
 
     actual = (t.summary, t.owner, t.done, t.id)
     expected = ('buy milk', 'brian', False, None)
 
     assert actual == expected
+
+
+def test_task_equality():
+    """Different tasks should not be equal."""
+    t1 = Task('sit there', 'brian')
+    t2 = Task('do something', 'okken')
+
+    assert t1 == t2
+
+
+def test_dict_equality():
+    """Different tasks compared as dicts should not be equal."""
+    t1_dict = Task('make sandwich', 'okken')._asdict()
+    t2_dict = Task('make sandwich', 'okkem')._asdict()
+
+    assert t1_dict == t2_dict
