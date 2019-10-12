ab716cc57adbf303b456ef50648918c4d91f6e50
Marc Clinedinst <msclin@clinedim.local>
2019-10-12 18:31:52 -0400

    Added some HTML report customization.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,15 @@
-"""Placeholder."""
+import pytest
 
-# nothing here yet
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item, call):
+    pytest_html = item.config.pluginmanager.get_plugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+    if report.when == 'cal':
+        extra.append(pytest_html.extras.url('http://www.example.com/'))
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
+        report.extra = extra
