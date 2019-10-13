bbf577e1ab88470b84ee079ffca0d92769dd74d0
Marc Clinedinst <msclin@clinedim.local>
2019-10-12 21:23:43 -0400

    Fixed error with test.

--- tests/func/test_api_exceptions.py
+++ tests/func/test_api_exceptions.py
@@ -6,5 +6,5 @@ import tasks
 @pytest.mark.exceptions
 def test_add_raises():
     """add() should raise an exception with wrong type param."""
-    with pytest.raises(ValueError):
+    with pytest.raises(TypeError):
         tasks.add(task='not a Task object')
